e1f1e4ad588d99a6c5277b5b2afab157518fef18
Abhishek Radhakrishnan <aradh@merit.edu>
2016-08-06 09:59:48 -0400

    added graphing -- databrick and bar (for top k)

--- edited_so.py
+++ edited_so.py
@@ -19,44 +19,88 @@ import logging
 import gflags
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, shared_queue=None):
         tk.Frame.__init__(self,master)
+        self.data_queue = shared_queue
         self.createWidgets()
 
-    def createWidgets(self):
-        fig=plt.figure(figsize=(8,8))
-        # ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
-        gs=GridSpec(3,1)
-        ax = fig.add_subplot(gs[0:2,:])
-        im = ax.imshow(np.random.random([128,128]), origin = 'upper', cmap=plt.cm.RdYlGn, interpolation = 'nearest')
-        topkplot = fig.add_subplot(gs[2,:])
-        bar_rect = plt.bar([1,2,3], [100,200,300], align='center', alpha=0.3, width=0.2, color='maroon')
-        canvas=FigureCanvasTkAgg(fig,master=root)
-        canvas.get_tk_widget().grid(row=0,column=1)
-        canvas.show()
 
-        self.plotbutton=tk.Button(master=root, text="plot", command=lambda: self.plot(canvas,ax,im))
+    def createWidgets(self):
+        self.fig=plt.figure(figsize=(10,10))
+        # ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+        self.gs= GridSpec(3,1)
+        ax = self.fig.add_subplot(self.gs[0:2,:])
+        self.im = ax.imshow(-np.random.random([128,128]), origin = 'upper', cmap=plt.cm.RdYlGn, interpolation = 'nearest', vmax=0, vmin=-40000)
+
+        self.num_graphed = 0
+
+        self.canvas = FigureCanvasTkAgg(self.fig,master=root)
+        self.canvas.get_tk_widget().grid(row=0,column=1)
+        self.canvas.show()
+
+        # self.plotbutton=tk.Button(master=root, text="plot", command=lambda: self.plot(canvas,ax,im))
+        self.plotbutton = tk.Button(master=root, text="plot", command=lambda: self.plot())
         self.plotbutton.grid(row=0,column=0)
 
         logging.info("Window configured!")
+        #return im
 
 
 
 
-    def plot(self,canvas,ax,im):
-        im.set_array(np.random.random([128,128]))
+    #def plot(self,canvas,ax,im):
+    # TODO(abhishek): change this method to pause/play!
+    def plot(self):
+        self.im.set_array(np.random.random([128,128]))
         bar_rect = plt.bar([5,0,19], [600,100,200], align='center', alpha=0.3, width=0.2, color='maroon')
         # im = ax.imshow(np.random.random([128,128]), origin = 'upper', cmap=plt.cm.RdYlGn, interpolation = 'nearest')
-        canvas.draw()
-        # ax.clear()
-        # for line in sys.stdout: #infinite loop, reads data of a subprocess
-        #     theta=line[1]
-        #     r=line[2]
-        #     ax.plot(theta,r,linestyle="None",maker='o')
-        #     canvas.draw()
-        #     ax.clear()
-        #     #here set axes
+        self.canvas.draw()
 
+
+
+
+    def Refresher(self):
+        #text.configure(text=time.asctime())
+
+        # cap_obj = self.data_queue.get(block=False)
+        cap_obj = self.data_queue.get()
+        if  cap_obj[0].size == 0:
+
+            root.after(1000, self.Refresher)
+        else:
+            # databrick sketch
+            num_intensities = cap_obj[0].reshape(128,128)
+            self.im.set_array(-num_intensities)
+
+            combined = []
+            for idx, src in enumerate(cap_obj[1]):
+                combined.append(src + "\n - \n" + cap_obj[2][idx])
+            y_pos = np.arange(len(combined))
+
+            # TODO(abhishek): Push this to below ==0: block (to avoid re-drawing all the time)?
+            self.topkplot = self.fig.add_subplot(self.gs[2,:])
+            self.topkplot.set_title('Top K hitters Stats')
+
+            if self.num_graphed == 0:
+
+                self.bar_rect = plt.bar(y_pos, cap_obj[3], align='center', alpha=0.3, width=0.2, color='maroon')
+            else:
+                #topkplot = self.fig.add_subplot(gs[2,:])
+                #topkplot.set_title('Top K hitters Stats')
+                for i in range(len(combined)):
+                    for rect, h in zip(self.bar_rect, cap_obj[3]):
+                        rect.set_height(h)
+
+            anno3 = self.topkplot.annotate('Current databrick visualising :%d'%(1), xy=(len(cap_obj[3]), 500000),horizontalalignment='right', verticalalignment='bottom')
+            plt.xticks(y_pos, combined)
+            plt.ylabel('# of Bytes')
+            plt.xlabel('Top %d hitters (src ip - dst ip)' %len(combined))
+
+            plt.pause(0.3)
+            anno3.remove()
+
+            self.canvas.draw()
+            root.after(1000, self.Refresher) # every second...
 
 
 class FlowtransmitImpl(ip_proto_capnp.Flowtransmit.Server):
@@ -70,8 +114,9 @@ class FlowtransmitImpl(ip_proto_capnp.Flowtransmit.Server):
         #logging.info("databrick rx: #of packets:%d"%sum(people))
         #converting to numpy array and send them to queue
         np_databrick = np.array(databrick)
+        print np_databrick
 
-        print np_databrick[0]
+        #print np_databrick[0]
         count_zeros = 0
         for d in range(len(np_databrick)):
             if np_databrick[d] == 0:
@@ -92,7 +137,7 @@ class FlowtransmitImpl(ip_proto_capnp.Flowtransmit.Server):
         logging.info("Putting data brick and hitters in the shared process queue")
         # logging.warn("CHECKKK Databrick:: %d"%(databrick))
 
-        #data_queue.put((np_databrick, src_hitters_li, dst_hitters_li, hitter_bytes))
+        data_queue.put((np_databrick, src_hitters_li, dst_hitters_li, hitter_bytes))
         # filter = interactive_filter_queue.get()
         # print "raw filter:",filter
         # filter = filter or 0
@@ -134,8 +179,13 @@ if __name__ == '__main__':
     logging.info("Cap'n Proto process started on port:%d"%port_number)
 
     root = tk.Tk()
-    app = Application(master=root)
+    app = Application(master=root, shared_queue=data_queue)
+    app.Refresher()
     app.mainloop()
+    # logging.info("Control never comes here")
+    # cap_conn_process.join()
+
+
 
     # mp_plot_process(data_queue, interactive_filter_queue)
     # cap_conn_process.join()
